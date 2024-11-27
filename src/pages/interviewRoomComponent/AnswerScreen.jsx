0a4a51bd9eabbeabe600029472727cfb70870793
an-jiohh <dkswlgh99@naver.com>
2024-11-27 17:11:37 +0900

    feat: 답변 화면 컴포넌트 추가 및 다음 질문 버튼 구현

---
+++ src/pages/interviewRoomComponent/AnswerScreen.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+import { useContext } from "react";
+import SetupContext from "@/pages/interviewRoomComponent/SetupProvider";
+
+const AnswerScreen = ({ onComplete }) => {
+  const { audioRef, videoRef, wsRef } = useContext(SetupContext);
+  return (
+    <div>
+      <h2>답변 화면</h2>
+      {/* 여기에 오디오 또는 비디오 녹음 설정 추가 */}
+      <button onClick={onComplete}>다음 질문</button>
+    </div>
+  );
+};
+
+export default AnswerScreen;
